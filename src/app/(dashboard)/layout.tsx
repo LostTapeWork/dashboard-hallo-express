1944442857fab53f30c88dda8bf45ecb8011cae7
Tedy Syach <tedyysyyach@gmail.com>
2024-03-04 19:57:20 +0700

    Revisi #2

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Epilogue } from "next/font/google";
-import "./globals.css";
+import "../globals.css";
 import SideBar from "@/components/sidebar";
 import Header from "@/components/header";
 
